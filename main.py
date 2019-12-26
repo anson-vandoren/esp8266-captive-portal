abb31facd2e8bc34e22213ca618d17fc73ab95fd
Anson VanDoren <anson.vandoren@gmail.com>
2019-12-26 11:29:07 -0800

    cleaning up

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import network
 import usocket as socket
 import uselect as select
-import uerrno
 import ubinascii as binascii
 import utime as time
 import uio
@@ -9,7 +8,9 @@ import gc
 
 gc.collect()
 
-LOCAL_IP = "192.168.4.1"
+local_ip = "192.168.4.1"
+AP_IF = network.WLAN(network.AP_IF)
+STA_IF = network.WLAN(network.STA_IF)
 
 ssid = None
 password = None
@@ -32,7 +33,7 @@ class DNSQuery:
             length = data[head]
 
     def answer(self, ip_addr):
-        # ** create the response header **
+        # ** create the answer header **
         # copy the ID from incoming request
         packet = self.data[:2]
         # set response flags (assume RD=1 from request)
@@ -42,7 +43,7 @@ class DNSQuery:
         # set NSCOUNT and ARCOUNT to 0
         packet += b"\x00\x00\x00\x00"
 
-        # ** create the response body **
+        # ** create the answer body **
         # respond with original domain name question
         packet += self.data[12:]
         # pointer back to domain name (at byte 12)
@@ -101,6 +102,8 @@ class DNSServer(Server):
 
             print("Sending {:s} -> {:s}".format(request.domain, self.ip_addr))
             sock.sendto(request.answer(self.ip_addr), sender)
+            del request
+            gc.collect()
         except Exception as e:
             print("DNS server exception:", e)
 
@@ -182,15 +185,12 @@ class HTTPServer(Server):
                 key, val = param.split(b"=")
                 wifi_params[key] = val
 
-            print("wifi params:", wifi_params)
             if b"ssid" in wifi_params and b"password" in wifi_params:
                 global ssid, password
                 ssid = wifi_params[b"ssid"]
                 password = wifi_params[b"password"]
-            if len(params) >= 2:
                 headers = b"HTTP/1.1 302 OK\r\nLocation: /authenticating"
                 base_path = b"/authenticating"
-
                 return headers, base_path
             else:
                 # wrong params
@@ -203,7 +203,7 @@ class HTTPServer(Server):
 
     def redirect(self, s):
         headers = b"HTTP/1.1 307 Temporary Redirect\r\n"
-        headers += b"Location: http://{:s}/".format(LOCAL_IP)
+        headers += b"Location: http://{:s}/".format(local_ip)
         body = uio.BytesIO(b"Please login first")
         self.write(s, body, headers)
         return None
@@ -234,15 +234,16 @@ class HTTPServer(Server):
             del self.conns[id(s)]
         gc.collect()
 
+    @micropython.native
     def handle(self, sock, event, others):
-        print("handling HTTP")
+        print("{:s} -- ".format(self.name))
         if sock is self.sock:
             # client connecting on port 80, so spawn off a new socket to handle this connection
-            print("\taccepting new incoming HTTP connection")
+            print("- accepting new incoming HTTP connection")
             self.accept(sock)
         elif event & select.POLLOUT:
             # an existing spawned socket has space to send more data
-            print("\tHTTP sending outgoing")
+            print("- HTTP sending outgoing")
             c = self.conns[id(sock)]
             if c:
                 w = sock.write(c[2][c[3][0] : c[3][1]])
@@ -251,52 +252,50 @@ class HTTPServer(Server):
                 else:
                     self.bufferfile(c, w)
             else:
-                print("\tfailed to find outgoing socket")
+                print("- failed to find outgoing socket")
         elif event & select.POLLIN:
-            print("\tHTTP reading incoming")
+            print("- HTTP reading incoming")
             self.read(sock)
 
-        print("done handling HTTP")
+        print("{:s} -- done".format(self.name))
         return True
 
 
 def configure_wan(mode="AP"):
     ap_mode = mode == "AP"
-    ap_if = network.WLAN(network.AP_IF)
-    sta_if = network.WLAN(network.STA_IF)
 
-    sta_if.active(not ap_mode)
+    STA_IF.active(not ap_mode)
 
     if ap_mode:
-        ap_if.active(False)
-        while not ap_if.active():
+        AP_IF.active(False)
+        while not AP_IF.active():
             print("waiting for AP mode to turn on")
-            ap_if.active(True)
+            AP_IF.active(True)
             time.sleep(1)
         # IP address, netmask, gateway, DNS
-        ap_if.ifconfig((LOCAL_IP, "255.255.255.0", LOCAL_IP, LOCAL_IP))
-        essid = b"ESP8266-%s" % binascii.hexlify(ap_if.config("mac")[-3:])
-        ap_if.config(essid=essid, authmode=network.AUTH_OPEN)
-        print("AP mode configured:", ap_if.ifconfig())
+        AP_IF.ifconfig((local_ip, "255.255.255.0", local_ip, local_ip))
+        essid = b"ESP8266-%s" % binascii.hexlify(AP_IF.config("mac")[-3:])
+        AP_IF.config(essid=essid, authmode=network.AUTH_OPEN)
+        print("AP mode configured:", AP_IF.ifconfig())
 
 
 def check_for_valid_wifi():
-    global ssid, password
+    global ssid, password, local_ip
     if ssid is None or password is None:
         return False
     print("Have WiFi Credentials, attempting connection...")
-    sta_if = network.WLAN(network.STA_IF)
-    sta_if.active(True)
-    sta_if.connect(ssid, password)
+    STA_IF.active(True)
+    STA_IF.connect(ssid, password)
     attempts = 0
     while attempts < 10:
-        if not sta_if.isconnected():
+        if not STA_IF.isconnected():
             print("Not connected yet")
             time.sleep(2)
             attempts += 1
         else:
             print("Connected to {:s}".format(ssid))
-            network.WLAN(network.AP_IF).active(False)
+            AP_IF.active(False)
+            local_ip = STA_IF.ifconfig()[0]
             gc.collect()
             return True
     configure_wan(mode="AP")
@@ -310,25 +309,31 @@ def captive_portal():
     global ssid, password
     ssid = password = None
     configure_wan(mode="AP")
-    # create a poller for both DNS and HTTP sockets
     poller = select.poll()
 
     http_server = HTTPServer(poller)
     http_server.routefile("/", "./index.html")
     http_server.routefile("/authenticating", "./authenticating.html")
-    dns_server = DNSServer(poller, LOCAL_IP)
+    dns_server = DNSServer(poller, local_ip)
+
     try:
         while not check_for_valid_wifi():
-            gc.collect()
-            for response in poller.ipoll(-1):
-                sock, event, *others = response
-                if sock is dns_server.sock and event == select.POLLHUP:
-                    continue
-                if sock is dns_server.sock:
-                    dns_server.handle(sock, event, others)
-                else:
-                    # all other sockets should belong to HTTP
-                    http_server.handle(sock, event, others)
+            if AP_IF.isconnected():
+                gc.collect()
+                for response in poller.ipoll(-1):
+                    sock, event, *others = response
+                    if sock is dns_server.sock and event == select.POLLHUP:
+                        continue
+                    if sock is dns_server.sock:
+                        dns_server.handle(sock, event, others)
+                    else:
+                        # all other sockets should belong to HTTP
+                        http_server.handle(sock, event, others)
+            else:
+                print("No clients connected to access point")
+                time.sleep(1)
+        else:
+            print("Connected with IP:", local_ip)
     except KeyboardInterrupt:
         print("Captive portal stopped")
     print("Cleaning up")
